034da9f5201ac05aaf0d5386b4330f871811e2c4
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2018-10-24 18:09:43 +0900

    Close #1: Make commitlang faster

--- Gemfile
+++ Gemfile
@@ -7,3 +7,5 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 # gem "rails"
 
 gem "github-linguist", "~> 7.0"
+
+gem "parallel", "~> 1.12"
--- bin/commitlang.rb
+++ bin/commitlang.rb
@@ -2,15 +2,19 @@
 
 require 'json/ext'
 require_relative "../lib/repowalker.rb"
+require 'parallel'
 
 def main(repo_path, email)
   walker = RepoWalker.new(repo_path, email)
-  walker.each { |c, lang|
-    puts({
-      'oid': c.oid,
-      'langages': lang.languages
-    }.to_json)
+  commits = []
+  walker.each { |c, lang| commits << [c, lang] }
+  results = Parallel.map(commits){ |el|
+    c = el[0]
+    lang = el[1]
+    { 'oid': c.oid, 'langages': lang.languages }.to_json
   }
+  results.map!{|s| JSON.parse s}
+  puts(results.to_json)
 end
 
 if __FILE__ == $0
