fb6cf362df12601673249e7bf9de6f834997118a
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-01-18 12:54:49 +0900

    Added debug code

--- .circleci/setup_gem.sh
+++ .circleci/setup_gem.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir ~/.gem
-echo -e "rubygems_api_key: ${GEM_KEY}" > ~/.gem/credentials
+echo -e "---\r\n:rubygems_api_key: ${GEM_KEY}" > ~/.gem/credentials
 chmod 0600 ~/.gem/credentials
